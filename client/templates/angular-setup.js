700595782e40f56868ea14a7aaa833aa9a2c0991
Ian MacFarland <ianmacfarland@ischool.berkeley.edu>
2014-11-29 14:03:10 -0800

    get & set question.answerable

--- client/templates/angular-setup.js
+++ client/templates/angular-setup.js
@@ -92,20 +92,58 @@ angular.module('data_qs').controller('QsController', ['$scope', '$collection', '
             }
             $scope.dataset.questions.push(new_question);
         }
+        $scope.answerable = function(q_id){
+            switch (_.findWhere($scope.dataset.questions, {id: q_id}).answerable) {
+                case true:
+                    return "ans true";
+                case false:
+                    return "ans false";
+                default:
+                    return "ans unknown";
+            }
+        }
+        $scope.answerableIcon = function(q_id){
+            switch (_.findWhere($scope.dataset.questions, {id: q_id}).answerable) {
+                case true:
+                    return "fa-check";
+                case false:
+                    return "fa-close";
+                default:
+                    return "fa-question";
+            }
+        }
     }]);
 
 angular.module('data_qs').controller('QuestController', ['$scope', '$collection', '$stateParams',
     function($scope, $collection, $stateParams){
         $collection(Datasets).bindOne($scope, 'dataset',
             {_id: $stateParams.datasetId}, true);
+
+        // sometimes the binding executes before meteor is fully initialized;
+        // the bindOne parameters are not all reactive. this should fix that
+        // https://github.com/Urigo/angular-meteor/issues/60
         $scope.$watch('dataset', function(val){
             if (val) {
                 if (val.questions) {
                     $scope.question = _.findWhere(val.questions,
                         {'id': $stateParams.questionId});
+
+                    $scope.isSet = function(ans_value){
+                        // if answerable state isn't set, fade the button
+                        if ($scope.question.answerable != ans_value) {
+                            return "fade";
+                        }
+                    };
+
+                    $scope.setAns = function(ans_value){
+                        console.log(val);
+                        $scope.question.answerable = ans_value;
+                    }
                 }
             }
         });
+
+
 
 
     }
