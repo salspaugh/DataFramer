fc9bc6b07ffc406086258de27efd04e615a62fad
Ian MacFarland <ianmacfarland@ischool.berkeley.edu>
2014-12-03 14:02:18 -0800

    fisheye spacing works right now

--- client/templates/angular-setup.js
+++ client/templates/angular-setup.js
@@ -1,4 +1,4 @@
-angular.module('data_qs',['angular-meteor', 'ui.router', 'd3']);
+angular.module('data_qs',['angular-meteor', 'ui.router']);
 
 Meteor.startup(function () {
   angular.bootstrap(document, ['data_qs']);
