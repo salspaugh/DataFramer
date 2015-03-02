e8aaef41fa4d20b32cde7b8f674728158a0cec60
Ian MacFarland <ianmacfarland@ischool.berkeley.edu>
2015-03-01 20:13:16 -0800

    new "start page" view
    - lists all datasets for current user (with links)
    - includes control to upload a new csv
    - displays message if there are no datasets for current user
    - prompts anons to log in
    - closes #40

--- client/controllers.js
+++ client/controllers.js
@@ -13,8 +13,43 @@ function($scope){
 // see _OLD DatasetsController, UploadController
 // ***********************************
 angular.module('dataFramer').controller('DatasetIndexController', ['$scope',
-function($scope){
+'$meteorCollection', '$meteorSubscribe',
+function($scope, $meteorCollection, $meteorSubscribe){
+    $scope.subReady = false;
 
+    $meteorSubscribe.subscribe('datasets').then(function(sub){
+        $scope.datasets = $meteorCollection(function(){
+            return Datasets.find({}, {fields: {name: 1}, sort: {name: 1}});
+        });
+
+        $scope.deleteDataset = function(){
+            Meteor.call('removeDataset', this.dataset._id);
+        }
+
+        $scope.subReady = true;
+    });
+
+    $scope.processCsv = function(event) {
+        var files = event.target.files;
+        for (var i = 0, ln = files.length; i < ln; i++) {
+            var file = files[i];
+            var reader = new FileReader();
+            reader.onload = function(event) {
+                var contents = event.target.result;
+                Meteor.call('processCsv', contents, file.name);
+            };
+
+            reader.onerror = function(event) {
+                console.error("File could not be read! Code " + event.target.error.code);
+            };
+
+            reader.readAsText(file);
+
+            // reset the upload form
+            event.target.value = '';
+        }
+    }
+    
 }]);
 
 
--- client/templates/dataset-index.tpl
+++ client/templates/dataset-index.tpl
@@ -1,2 +1,30 @@
 <!-- the first-time start page, the list of datasets, and the upload form -->
 <!-- see _OLD datasets-list.tpl, upload.tpl for usable pieces -->
+<div class="col-md-8 col-md-offset-2">
+    <div ng-if="$root.currentUser">
+    <section class="page-header">
+        <h1>Your Datasets</h1>
+    </section>
+
+    <section class="list-group">
+        <a class="list-group-item" ng-repeat="dataset in datasets" ui-sref="dataset.questionIndex({datasetId: dataset._id})">
+            <h4 class="list-group-item-heading">{{dataset.name}}</h4>
+        </a>
+        <section ng-hide="subReady" class="list-group-item">
+            <i class="fa fa-spinner fa-spin fa-3x"></i>
+        </section>
+        <section class="list-group-item disabled" ng-if="subReady && datasets.length == 0">
+            <h4 class="list-group-item-heading">You haven't uploaded any datasets yet.</h4>
+        </section>
+        <section class="list-group-item list-group-item-info">
+            <span class="btn btn-primary btn-block btn-file">
+                <strong>Upload a new dataset (.csv files only)</strong>
+                <input type="file" onchange="angular.element(this).scope().processCsv(event)"/>
+            </span>
+        </section>
+    </section>
+    </div>
+    <div ng-if="!$root.currentUser">
+        <h2>Please sign in or create an account to start using DataFramer.</h2>
+    </div>
+</div>
